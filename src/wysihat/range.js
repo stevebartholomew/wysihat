5069665b90775917d9f88aee0bc2532086947010
Joshua Peek <josh@joshpeek.com>
2008-10-28 14:46:53 -0500

    import w3c docs for range

--- src/wysihat/range.js
+++ src/wysihat/range.js
@@ -38,10 +38,19 @@ if (Prototype.Browser.IE) {
   }
 
   Object.extend(Range.prototype, (function() {
+    /**
+     * Range#cloneContents() -> DocumentFragment
+     *  Duplicates the contents of a Range
+    **/
     function cloneContents() {
       return _processContents(this, Range.CLONE_CONTENTS);
     }
 
+    /**
+     * Range#cloneRange() -> Range
+     *  Produces a new Range whose boundary-points are equal to the
+     *  boundary-points of the Range.
+    **/
     function cloneRange() {
       try {
         var clone = new Range(this.ownerDocument);
@@ -61,6 +70,12 @@ if (Prototype.Browser.IE) {
       };
     }
 
+    /**
+     * Range#collapse(toStart) -> undefined
+     *  - toStart (Boolean): If TRUE, collapses the Range onto its start;
+     *      if FALSE, collapses it onto its end.
+     *  Collapse a Range onto one of its boundary-points
+    **/
     function collapse(toStart) {
       if (toStart) {
         this.endContainer = this.startContainer;
@@ -73,6 +88,13 @@ if (Prototype.Browser.IE) {
       }
     }
 
+    /**
+     * Range#compareBoundaryPoints(how, sourceRange) -> Integer
+     *  - how (Integer): A code representing the type of comparison
+     *  - sourceRange (Range): The Range on which this current Range is
+     *      compared to.
+     *  Compare the boundary-points of two Ranges in a document.
+    **/
     function compareBoundaryPoints(compareHow, sourceRange) {
       try {
         var cmnSelf, cmnSource, rootSelf, rootSource;
@@ -110,16 +132,33 @@ if (Prototype.Browser.IE) {
       return null;
     }
 
+    /**
+     * Range#deleteContents() -> undefined
+     *  Removes the contents of a Range from the containing document or
+     *  document fragment without returning a reference to the
+     *  removed content.
+    **/
     function deleteContents() {
       try {
         _processContents(this, Range.DELETE_CONTENTS);
       } catch (e) {}
     }
 
+    /**
+     * Range#detach() -> undefined
+     *  Called to indicate that the Range is no longer in use and that the
+     *  implementation may relinquish any resources associated with
+     *  this Range.
+    **/
     function detach() {
       this.detached = true;
     }
 
+    /**
+     * Range#extractContents() -> DocumentFragment
+     *  Moves the contents of a Range from the containing document or
+     *  document fragment to a new DocumentFragment.
+    **/
     function extractContents() {
       try {
         return _processContents(this, Range.EXTRACT_CONTENTS);
@@ -128,6 +167,18 @@ if (Prototype.Browser.IE) {
       };
     }
 
+    /**
+     * Range#insertNode(newNode) -> undefined
+     *  - newNode (Node): The node to insert at the start of the Range
+     *  Inserts a node into the Document or DocumentFragment at the start
+     *  of the Range. If the container is a Text node, this will be split at
+     *  the start of the Range (as if the Text node's splitText method was
+     *  performed at the insertion point) and the insertion will occur
+     *  between the two resulting Text nodes. Adjacent Text nodes will not
+     *  be automatically merged. If the node to be inserted is a
+     *  DocumentFragment node, the children will be inserted rather than the
+     *  DocumentFragment node itself.
+    **/
     function insertNode(newNode) {
       try {
         var n, newText, offset;
@@ -149,16 +200,33 @@ if (Prototype.Browser.IE) {
       } catch (e) {}
     }
 
+    /**
+     * Range#selectNode(refNode) -> undefined
+     *  - refNode (Node): The node to select.
+     *  Select a node and its contents
+    **/
     function selectNode(refNode) {
       this.setStartBefore(refNode);
       this.setEndAfter(refNode);
     }
 
+    /**
+     * Range#selectNodeContents(refNode) -> undefined
+     *  - refNode (Node): Node to select from
+     *  Select the contents within a node
+    **/
     function selectNodeContents(refNode) {
       this.setStart(refNode, 0);
       this.setEnd(refNode, refNode.childNodes.length);
     }
 
+    /**
+     * Range#setStart(refNode, offset) -> undefined
+     *  - refNode (Node): The refNode value. This parameter must be different
+     *      from null.
+     *  - offset (Integer): The endOffset value.
+     *  Sets the attributes describing the end of a Range.
+    **/
     function setStart(refNode, offset) {
       try {
         var endRootContainer, startRootContainer;
@@ -196,14 +264,31 @@ if (Prototype.Browser.IE) {
       } catch (e) {}
     }
 
+    /**
+     * Range#setStartAfter(refNode) -> undefined
+     *  - refNode (Node): Range ends after refNode.
+     *  Sets the end of a Range to be after a node
+    **/
     function setStartAfter(refNode) {
       this.setStart(refNode.parentNode, _nodeIndex(refNode) + 1);
     }
 
+    /**
+     * Range#setStartBefore(refNode) -> undefined
+     *  - refNode (Node): Range ends before refNode
+     *  Sets the end position to be before a node.
+    **/
     function setStartBefore(refNode) {
       this.setStart(refNode.parentNode, _nodeIndex(refNode));
     }
 
+    /**
+     * Range#setEnd(refNode, offset) -> undefined
+     *  - refNode (Node): The refNode value. This parameter must be different
+     *      from null.
+     *  - offset (Integer): The startOffset value.
+     *  Sets the attributes describing the start of the Range.
+    **/
     function setEnd(refNode, offset) {
       try {
         this.endContainer = refNode;
@@ -238,14 +323,30 @@ if (Prototype.Browser.IE) {
       } catch (e) {}
     }
 
+    /**
+     * Range#setEndAfter(refNode) -> undefined
+     *  - refNode (Node): Range starts after refNode
+     *  Sets the start position to be after a node
+    **/
     function setEndAfter(refNode) {
       this.setEnd(refNode.parentNode, _nodeIndex(refNode) + 1);
     }
 
+    /**
+     * Range#setEndBefore(refNode) -> undefined
+     *  - refNode (Node): Range starts before refNode
+     *  Sets the start position to be before a node
+    **/
     function setEndBefore(refNode) {
       this.setEnd(refNode.parentNode, _nodeIndex(refNode));
     }
 
+    /**
+     * Range#surroundContents(newParent) -> undefined
+     *  - newParent (Node): The node to surround the contents with.
+     *  Reparents the contents of the Range to the given node and inserts the
+     *  node at the position of the start of the Range.
+    **/
     function surroundContents(newParent) {
       try {
         var n, fragment;
